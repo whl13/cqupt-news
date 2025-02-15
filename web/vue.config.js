1b68f9bb1c2792052475b643b554e5b0a2d1d005
姜敏 <66110126+whl13@users.noreply.github.com>
2025-02-15 15:01:22 +0800

    3

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',
   devServer: {
     proxy: {
       '/webapi': {
