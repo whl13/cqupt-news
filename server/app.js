369105d1c7ad97f07824cc3f4d45fa21735c55bf
姜敏 <66110126+whl13@users.noreply.github.com>
2025-03-22 10:55:22 +0800

    node

--- server/app.js
+++ server/app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const webnewsRouter = require('./routes/web/webnewsRouter');
 const userRouter = require('./routes/admin/userRouter');
 const newsRouter = require('./routes/admin/newsRouter');
@@ -14,18 +12,19 @@ const productRouter = require('./routes/admin/productRouter');
 const JWT = require('./utils/JWT')
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// 将jade设置为默认模板引擎(模板需要经过render后才能使用)
 app.set('view engine', 'jade');
+// 设置模板路径
+app.set('views', path.join(__dirname, 'views'));
+
 
 app.use(logger('dev'));
 app.use(express.json());
+// 解析req.body
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// 设置静态资源路径
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use(webnewsRouter)
 app.use(webproductRouter)
 app.use((req, res, next) => {
@@ -36,7 +35,6 @@ app.use((req, res, next) => {
     const token = req.headers.authorization.replace('Bearer ', '')
     if (token) {
       const payload = JWT.verify(token)
-      console.log(payload)
       if (payload) {
         const newToken = JWT.generate({
           _id: payload._id,
@@ -67,8 +65,15 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+
   res.status(err.status || 500);
+  /* 通过render渲染error.jade模板并返回给浏览器
+  render('模板名',{a:1,b:'sth'})
+  可以通过第二个参数传对象给模板使用(类似于vue，react)
+  模板中通过<%=变量名 %> (经过转义的) 
+           <%-变量名 %> (未经转义的,原样输出包括html标签,易xss攻击)来使用
+           <% js代码 %> 在模板中编写js代码，在服务器端运行  
+  */
   res.render('error');
 });
 
